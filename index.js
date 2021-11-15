b01beb6c2f222aa3ca2c290773feb0685c4f41db
Rocklee0 <mshakil3847@gmail.com>
2021-11-16 03:43:04 +0600

    get fixed

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ async function run(){
         //GET BY EMAIL BOOKING 
         app.get('/purchase',async(req,res)=>{
             const email = req.query.Email;
-            const query = { Email: Email};
+            const query = { Email: email};
             const cursor = orderCollection.find(query);
             const orders=await cursor.toArray();
             res.json(orders);
